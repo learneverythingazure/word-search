add04bd10dce409cc087c9844f6606d9a4910807
learneverythingazure <learneverythingazure@gmail.com>
2023-01-26 05:02:20 +0000

    Created a board, sort of.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+function Board({ cells }) {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      {cells.map((item) => (
+        <Square value={item} />
+      ))}
+    </>
   );
+}
+
+export default function App() {
+  const [cells] = useState([Array(9).fill("B")]);
+  return <Board cells={cells} />;
 }
