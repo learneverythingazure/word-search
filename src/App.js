af1a715d03a138b5dc3aa3c5737b438b7940431b
learneverythingazure <learneverythingazure@gmail.com>
2023-01-26 06:46:27 +0000

    Filled board

--- src/App.js
+++ src/App.js
@@ -25,11 +25,66 @@ function random_letter() {
   return upper_letters.charAt(i);
 }
 
-export default function App() {
-  const [cells] = useState(
-    Array(9)
-      .fill("A")
-      .map(() => Array(9).fill("A"))
-  );
+function random_direction() {
+  return Math.random() < 0.5;
+}
+
+function random_coordinate(numRows, numCols) {
+  const row = Math.floor(Math.random() * numRows);
+  const col = Math.floor(Math.random() * numCols);
+  return [row, col];
+}
+
+function get_words() {
+  return ["ALPHA", "BRAVO", "CHARLIE", "DELTA", "ECHO", "FOXTROT"];
+}
+
+function place_word(numRows, numCols, cells, word) {
+  const isVertical = random_direction();
+  const [row, col] = random_coordinate(numRows, numCols);
+
+  if (isVertical && numRows - row >= word.length) {
+    for (let i = 0; i < word.length; i++) {
+      if (cells[row + i][col] !== " " && cells[row + i][col] !== word[i]) {
+        return;
+      }
+    }
+    [...word].map((c, i) => (cells[row + i][col] = c));
+  }
+
+  if (!isVertical && numCols - col >= word.length) {
+    for (let i = 0; i < word.length; i++) {
+      if (cells[row][col + i] !== " " && cells[row][col + i] !== word[i]) {
+        return;
+      }
+    }
+    [...word].map((c, i) => (cells[row][col + i] = c));
+  }
+}
+
+function generate_cells() {
+  const [numRows, numCols] = [12, 12];
+  const cells = Array(numRows)
+    .fill(" ")
+    .map(() => new Array(numCols).fill(" "));
+  const words = get_words();
+
+  words.map((word) => place_word(numRows, numCols, cells, word));
+
+  for (let row = 0; row < numRows; row++) {
+    for (let col = 0; col < numCols; col++) {
+      if (cells[row][col] === " ") {
+        cells[row][col] = random_letter();
+      }
+    }
+  }
+
+  return cells;
+}
+
+export default function Game() {
+  const [numRows, numCols] = [12, 12];
+  const [cells] = useState(generate_cells());
+
   return <Board cells={cells} />;
 }
